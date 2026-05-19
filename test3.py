55ad73da75d4941e7168a34ca5097816887f095f
tsapp31 <taylor.sapp@gtri.gatech.edu>
2026-05-19 12:12:21 -0400

    Update test3.py

--- test3.py
+++ test3.py
@@ -4,13 +4,14 @@ import openpyxl
 data = pd.read_excel("test.xlsx")
 print(data)
 
-from openpyxl import load_workbook
-data = load_workbook(filename="test.xlsx")
-sheet = data.active
-for name in sheet["B"]:
-    num = name.row
-    if name == "Sapp":
-        sheet[f"B{num}"] = "Sapp family"
-    else:
-        sheet[f"B{num}"] = "Not Sapp family"
-data.save("test.xlsx")
+# from openpyxl import load_workbook
+# data = load_workbook(filename="test.xlsx")
+# sheet = data.active
+# for name in sheet["B"]:
+    # num = name.row
+    # if name == "Sapp":
+    #     sheet[f"B{num}"] = "Sapp family"
+    # else:
+    #     sheet[f"B{num}"] = "Not Sapp family"
+# data.save("test.xlsx")
+
