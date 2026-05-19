ebf2de6096a7bb28ba4d091d1b6b7b54c5f3e8d0
tsapp31 <taylor.sapp@gtri.gatech.edu>
2026-05-19 12:04:36 -0400

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,16 @@
+# Test 3: getting info from Excel
+import pandas as pd
+import openpyxl
+data = pd.read_excel("test.xlsx")
+print(data)
+
+from openpyxl import load_workbook
+data = load_workbook(filename="test.xlsx")
+sheet = data.active
+for name in sheet["B"]:
+    num = name.row
+    if name == "Sapp":
+        sheet[f"B{num}"] = "Sapp family"
+    else:
+        sheet[f"B{num}"] = "Not Sapp family"
+data.save("test.xlsx")
