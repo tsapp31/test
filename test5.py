d879c8d73230f236a5c11fab08055ab69a3315ac
tsapp31 <taylor.sapp@gtri.gatech.edu>
2026-05-21 14:39:51 -0400

    Create test5.py

---
+++ test5.py
@@ -0,0 +1,18 @@
+# Test 5: python libraries
+
+# Internal libraries
+import math
+import os
+import datetime
+import random
+import json
+
+# External libraries
+from numpy import array, mean
+import pandas as pd
+
+# Examples
+A = 16
+print(math.sqrt(A))
+a = array([10,20,30,40,50])
+print(mean(a))
